fb3f3acf1cad73235fd490d986c6f2c652a3121b
Maxime GARIBALDI <mgaribal@insa-rennes.fr>
2025-12-27 18:20:19 +0100

    V4 portfolio

---
+++ src/app/carousel/carousel.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  imports: [],
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.css',
+})
+export class Carousel {
+  index = 0;
+
+  pdfs = [
+    { img: 'CV.png', pdf: 'CV.pdf', alt: 'CV Français' },
+    { img: 'CV.png', pdf: 'CV.pdf', alt: 'CV Anglais' }
+  ];
+
+  next() {
+    this.index = (this.index + 1) % this.pdfs.length;
+  }
+
+  prev() {
+    this.index = (this.index - 1 + this.pdfs.length) % this.pdfs.length;
+  }
+
+  open(pdf: string) {
+    window.open(pdf, '_blank');
+  }
+
+}
--- src/app/pages/welcome/welcome.ts
+++ src/app/pages/welcome/welcome.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Carousel } from '../../carousel/carousel';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [Carousel],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
 export class Welcome {
+  constructor(private router: Router) {}
+
+  goToProj() {
+    this.router.navigate(['/projects']);
+  }
+
+  goToIntern() {
+    this.router.navigate(['/internships']);
+  }
+
+  goToHobb() {
+    this.router.navigate(['/hobbies']);
+  }
+
+  goToCivil() {
+    this.router.navigate(['/civil-engagement']);
+  }
 
 }
